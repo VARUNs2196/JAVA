b5083eacb642a48e8a5dd2f6b91d9afe874cb085
VARUN SACHDEVA <VARUN.15987@stu.upes.ac.in>
2024-09-27 23:42:19 +0530

    Push debug.java

---
+++ Debug.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Debug{
+    public static void main(String[] args) {
+        System.out.println("hello");
+        Scanner sc=new Scanner(System.in);
+        String name=sc.next();
+        StringBuffer sb=new StringBuffer(name.charAt(0));
+        for(int i=0;i<name.length();i++){
+            if("" + name.charAt(i).equals"") {
+                sb.append(name.charAt(i+1));
+            }
+        }
+        System.out.println(sb);
+    }
+}
+// HashSet<Integer> Set = new HashSet<>();
+// for(i=0;i<n;i++)
+// {
+//     if(Set.contains(Sum-a[i])){
+//         return True;
+//     }
+//     Set add(ar[i]);
+
+// }
+// return False;
+3+1+2+4+2=4=2=4
+x=4
+y=2
+z=4
